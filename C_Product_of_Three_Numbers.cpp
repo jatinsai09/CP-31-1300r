fac8fdc11c8b058eb313145d850e2534d56245a6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-01 22:12:44 +0530

    Add files via upload

---
+++ C_Product_of_Three_Numbers.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define int long long int
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a, b, c;
+
+        for (a = 2; a * a * a <= n; a++) {
+            if (n % a == 0) {
+                break;
+            }
+        }
+        int rem = n / a;
+
+        for (b = a + 1; b * b <= rem; b++) {
+            if (rem % b == 0) {
+                c = rem / b;
+                if (b >= c) {
+                    c = 0;
+                }
+                break;
+            }
+        }
+
+        if (a * b * c == n) {
+            cout << "YES\n";
+
+            cout << a << " " << b << " " << c << endl;
+        }
+        else {
+            cout << "NO\n";
+        }
+
+    }
+}
